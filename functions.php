2afc4337e137109db46c1485d2befc753c887ba8
lucky <renbaoyong@gmail.com>
2013-05-17 13:13:09 +0800

    初始化

---
+++ functions.php
@@ -0,0 +1,51 @@
+<?php
+//*************************里面的内容根据自己实际情况自行修改**************************// 	
+	$_hostname	= 'localhost'; 
+	$_username	= 'root';    	//你的数据库的用户名
+	$_password	= '111111';		//你的数据库的密码
+	$_dbname	= "yourdb";	    //数据库的名称
+
+	//用户关注后的欢迎词，根据自己的微信公众账号修改
+	$WELCOME_MSG = "很高兴您使用本系统，我们将为您提供更好的信息服务，您可以输入?获取更多帮助";
+	//用户输入的关键词系统中还没有 ，进行信息提示
+	$NO_RESPONSE = "我们暂时还不提供您要查询的内容，请输入?,获取更多的内容";
+//***********************************************************************************//	
+
+	
+    $textTpl 	=  "<xml>
+					<ToUserName><![CDATA[%s]]></ToUserName>
+					<FromUserName><![CDATA[%s]]></FromUserName>
+					<CreateTime>%s</CreateTime>
+					<MsgType><![CDATA[%s]]></MsgType>
+					<Content><![CDATA[%s]]></Content>
+					<FuncFlag>0</FuncFlag>
+					</xml>";  
+	$conn = mysql_connect($_hostname, $_username, $_password);
+	mysql_select_db($_dbname, $conn);
+	mysql_query("set names utf-8");// connect to db
+ 
+ 	//自定义的方法都可以写到这个里面来
+	function show($keyword, $key, $fromUsername, $toUsername)
+	{
+		
+		$textTpl 	=  "<xml>
+		<ToUserName><![CDATA[%s]]></ToUserName>
+		<FromUserName><![CDATA[%s]]></FromUserName>
+		<CreateTime>%s</CreateTime>
+		<MsgType><![CDATA[%s]]></MsgType>
+		<Content><![CDATA[%s]]></Content>
+		<FuncFlag>0</FuncFlag>
+		</xml>";  
+		$time 		= time();
+		$msgType 	= "text";
+		$contentStr	= "您输入的是，这是一个自定义的方法：".$keyword;
+		$resultStr 	= sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+		echo $resultStr;
+		mysql_query("insert into messages (uid,content) values('{$fromUsername}','{$keyword}')");
+	}
+	
+	function yourfunction($keyword, $key, $fromUsername, $toUsername)
+	{
+		//..........
+	}
+?>
---
+++ weixin.php
@@ -0,0 +1,145 @@
+<?php
+  require_once('functions.php');
+  $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+ 
+  if (!empty($postStr)){
+  	$postObj 	= simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+	$mType 		= $postObj->MsgType;
+
+		if($mType =='text')
+		{
+			$fromUsername 	= $postObj->FromUserName;
+			$toUsername 	= $postObj->ToUserName;
+			$keyword 		= trim($postObj->Content);
+			$time 			= time();
+   			if(!empty( $keyword ))
+            {  
+				   /*
+				    先去数据库中查找是否有匹配的关键词
+				    再查是否有以此关键词为开头的关键词
+				    */
+				    $sql="select keyword,title,description,type,picurl,url from articles where keyword='{$keyword}' and keyword_type='equals'";
+				    $result=mysql_query($sql); 
+					$num = mysql_num_rows($result);
+					if($num<1){
+						  		$sql="select keyword,title from articles where keyword_type='startwith'";
+				             	$result=mysql_query($sql); 
+				 
+				             	$starwith		=	false;
+				             	$starwith_str	=	"";
+				             	$starwith_func	= 	"";
+				             	while ($row=mysql_fetch_row($result))
+								{
+				 					$_keyword		= 	$row[0];
+									$len 			= 	strlen($_keyword);
+									if( substr($keyword,0,$len) == $_keyword )
+									{
+										$starwith_str 	= $_keyword;
+										$starwith_func	= $row[1];
+										$starwith		= true;
+										break;
+									}
+								}
+								
+								if( $starwith )
+								{
+									 $starwith_func($keyword, $starwith_str, $fromUsername, $toUsername);
+								}
+								else
+								{
+									$contentStr	=	$NO_RESPONSE;
+									
+									$msgType 	= "text";
+									$resultStr 	= sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+									echo $resultStr;	
+								}
+				 
+					}else
+					{
+						//有匹配的数据
+						$row			=	mysql_fetch_row($result);
+						$type 			=	$row[3];
+						$description 	=	$row[2];
+						$time 			=  time();
+						$picurl 		=	$row[4];
+						$url 			=	$row[5];
+						$url = str_replace("[[wxid]]", $fromUsername, $url);		// 替换微信的ID
+						if( "text" == $type )
+						{
+							$contentStr	=	$description;
+							$contentStr = str_replace("[[wxid]]", $fromUsername, $contentStr);		// 替换微信的ID
+							$msgType 	= "text";
+							$resultStr 	= sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+							echo $resultStr;
+						}
+						if( "news" == $type )
+						{
+									$newsTpl = "<xml>
+									<ToUserName><![CDATA[%s]]></ToUserName>
+									<FromUserName><![CDATA[%s]]></FromUserName>
+									<CreateTime>%s</CreateTime>
+									<MsgType><![CDATA[%s]]></MsgType>
+									<ArticleCount>1</ArticleCount>
+									<Articles>
+									<item>
+									<Title><![CDATA[%s]]></Title>
+									<Description><![CDATA[%s]]></Description>
+									<PicUrl><![CDATA[%s]]></PicUrl>
+									<Url><![CDATA[%s]]></Url>
+									</item>
+									</Articles>
+									<FuncFlag>1</FuncFlag>
+									</xml>"; 
+									$msgType1  = "news";
+									$resultStr = sprintf($newsTpl, $fromUsername, $toUsername, $time, $msgType1, $title,$description,$picurl,$url);
+									echo $resultStr;
+						}
+						mysql_query("insert into messages (uid,content) values('{$fromUsername}','{$keyword}')");
+					}
+            }
+	              	 
+		                
+	    }elseif($mType =='event')
+	    {
+	    	$fromUsername = $postObj->FromUserName;
+			$toUsername = $postObj->ToUserName;
+			$location_x = $postObj->Location_X;
+			$location_y = $postObj->Location_Y;
+			$scale = $postObj->Scale;
+			$label = $postObj->Label;
+			$time = time();
+
+			 $contentStr =$WELCOME_MSG;   
+			 $msgType = "text";
+			 $sql1="select * from sj where wxid='{$fromUsername}'";
+             $result1=mysql_query($sql1);
+             $num1 = mysql_num_rows($result1);
+			 if($num1<1)
+			 mysql_query("insert into sj (wxid) values('{$fromUsername}')");
+			
+			$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+   			echo $resultStr;
+	    }
+	    elseif($mType =='location')
+	    {
+			$fromUsername = $postObj->FromUserName;
+			$toUsername = $postObj->ToUserName;
+			$location_x = $postObj->Location_X;
+			$location_y = $postObj->Location_Y;
+			$scale = $postObj->Scale;
+			$label = $postObj->Label;
+			$time = time();
+			$contentStr = "您更新了您的位置信息";              
+			$msgType = "text";
+			$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+			mysql_query("update sj set x='{$location_x}',y='{$location_y}',dz='{$label}',last_update='{$time}' where wxid='{$fromUsername}'");
+			echo $resultStr;
+	     }
+       
+	}else {
+		echo "";
+		exit;
+	}
+
+
+?>
